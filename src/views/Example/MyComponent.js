88fe89f9fea854c7f7f8626b43ba295560b871e3
Nguyen Duc Manh <davidnguyen10323@gmail.com>
2024-08-02 23:11:39 +0700

    state

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -2,9 +2,22 @@ import React from 'react';
 
 class MyComponent extends React.Component {
 
+    state = {
+        name: 'Manh',
+        chanel: 'hoi dan it'
+    }
+
     render() {
+
         return (
-            <div>hello my Component</div>
+            <>
+                <div className="first">
+                    hello my name is {this.state.name}
+                </div>
+                <div className="second">
+                    My chanel: {this.state.chanel}
+                </div>
+            </>
         )
     }
 
